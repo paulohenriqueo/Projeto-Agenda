ca167db1077ffac36ffe8fd3e1d004982f4d870d
paulohenriqueo <paulohenrique300305@gmail.com>
2024-06-13 23:17:39 -0300

    :sparkles: feat: componentes e rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { AgendaComponent } from './agenda/agenda.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',           component: HomeComponent},
+  { path: 'cadastro',   component: CadastroComponent},
+  { path: 'agenda',     component: AgendaComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { AgendaComponent } from './agenda/agenda.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AgendaComponent,
+    CadastroComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
