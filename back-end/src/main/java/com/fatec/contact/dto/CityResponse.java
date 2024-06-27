4aecb8b9bb7e77e611ab819da6092bdef8bfc74b
paulohenriqueo <paulohenrique300305@gmail.com>
2024-06-26 23:18:35 -0300

    :sparkles: feat: DTO Request de Response

---
+++ back-end/src/main/java/com/fatec/contact/dto/CityResponse.java
@@ -0,0 +1,6 @@
+package com.fatec.contact.dto;
+
+public record CityResponse(int id, String name, String email, String sex,
+                            String choose, String phone, String speci) {
+    
+}
