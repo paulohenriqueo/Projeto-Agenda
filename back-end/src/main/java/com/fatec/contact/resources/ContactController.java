f7ac15774679ad031fba9226c9e74e2a26edcd3a
paulohenriqueo <paulohenrique300305@gmail.com>
2024-06-27 10:06:02 -0300

    :sparkles: feat: CrossOrigin

--- back-end/src/main/java/com/fatec/contact/resources/ContactController.java
+++ back-end/src/main/java/com/fatec/contact/resources/ContactController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,7 @@ import com.fatec.contact.dto.ContactResponse;
 import com.fatec.contact.resources.services.ContactService;
 
 @RestController
+@CrossOrigin
 @RequestMapping("contact")
 public class ContactController {
     
