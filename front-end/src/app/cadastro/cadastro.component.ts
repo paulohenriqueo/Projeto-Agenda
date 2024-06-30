66e4f695583417886b1d96c0d497b9704994cba2
paulohenriqueo <paulohenrique300305@gmail.com>
2024-06-30 00:18:39 -0300

    :sparkles: feat: validações e mensagens de erro

--- front-end/src/app/cadastro/cadastro.component.ts
+++ front-end/src/app/cadastro/cadastro.component.ts
@@ -1,66 +1,83 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ContatoService } from '../contato.service';
-import { Contact} from '../contact';
+import { Contact } from '../contact';
 
 @Component({
   selector: 'app-cadastro',
   templateUrl: './cadastro.component.html',
-  styleUrl: './cadastro.component.css'
+  styleUrl: './cadastro.component.css',
 })
 export class CadastroComponent implements OnInit {
-
-
   contacts: Contact[] = [];
-  formGroupContact : FormGroup;
+  formGroupContact: FormGroup;
   isEditing: boolean = false;
+  submited: boolean = false;
 
-  constructor(private formBuilder : FormBuilder,
-              private service : ContatoService){
-                this.formGroupContact = formBuilder.group({
-                  id : [''],
-                  name : [''],
-                  email : [''],
-                  sex : [''],
-                  choose : [''],
-                  phone : [''],
-                  speci : [false],
-                })
-              }
-  
+  constructor(
+    private formBuilder: FormBuilder,
+    private service: ContatoService
+  ) {
+    this.formGroupContact = formBuilder.group({
+      id: [''],
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required]],
+      sex: [''],
+      choose: [''],
+      phone: ['', [Validators.required, Validators.minLength(10)]],
+      speci: ['Contato Pessoal'],
+    });
+  }
+
   ngOnInit(): void {
     this.loadContacts();
   }
 
   loadContacts() {
     this.service.getContacts().subscribe({
-      next : data => this.contacts = data
+      next: (data) => (this.contacts = data),
     });
   }
 
-  save(){
-    if (this.isEditing) {
-      this.service.update(this.formGroupContact.value).subscribe({
-        next : () => {
-          this.loadContacts();
-          this.isEditing = false;
-        }
-      })
+  save() {
+    this.submited = true;
+    if (this.formGroupContact.valid) {
+      if (this.isEditing) {
+        this.service.update(this.formGroupContact.value).subscribe({
+          next: () => {
+            this.loadContacts();
+            this.isEditing = false;
+          },
+        });
+      } else {
+        this.service.save(this.formGroupContact.value).subscribe({
+          next: (data) => {
+            this.contacts.push(data);
+            this.submited = false;
+          },
+        });
+      }
     }
-    else{
-      this.service.save(this.formGroupContact.value).subscribe({
-        next : (data: Contact) => this.contacts.push(data)
-      });
-    }
+
     this.formGroupContact.reset();
   }
-  delete(contact : Contact) {
+  delete(contact: Contact) {
     this.service.delete(contact).subscribe({
-      next : () => this.loadContacts()
-    })
-    }
-   edit(contact : Contact){
+      next: () => this.loadContacts(),
+    });
+  }
+  edit(contact: Contact) {
     this.formGroupContact.setValue(contact);
     this.isEditing = true;
-   } 
+  }
+
+  get name(): any {
+    return this.formGroupContact.get('name');
+  }
+  get email(): any {
+    return this.formGroupContact.get('email');
+  }
+  get phone(): any {
+    return this.formGroupContact.get('phone');
+  }
 }
--- front-end/src/app/contato.service.ts
+++ front-end/src/app/contato.service.ts
@@ -4,28 +4,26 @@ import { Contact } from './contact';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ContatoService {
+  url = 'http://localhost:8080/contacts';
 
-  url = "http://localhost:8080/contacts"
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
-
-  getContacts() : Observable<Contact[]>{
+  getContacts(): Observable<Contact[]> {
     return this.http.get<Contact[]>(this.url);
   }
 
-  save(contact : Contact): Observable<Contact>{
+  save(contact: Contact): Observable<Contact> {
     return this.http.post<Contact>(this.url, contact);
   }
 
-  delete(contact: Contact): Observable<void>{
-    return this.http.delete<void>(`${this.url}/${contact.id}`)
+  delete(contact: Contact): Observable<void> {
+    return this.http.delete<void>(`${this.url}/${contact.id}`);
   }
 
-  update(contact : Contact): Observable<Contact>{
-    return this.http.put<Contact>(`${this.url}/${contact.id}`,contact);
+  update(contact: Contact): Observable<Contact> {
+    return this.http.put<Contact>(`${this.url}/${contact.id}`, contact);
   }
-
 }
